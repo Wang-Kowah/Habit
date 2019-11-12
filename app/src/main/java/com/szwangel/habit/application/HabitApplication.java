b1fa2e4a6bf22c164115d2ad1ac9164933b8b8bf
Kowah <1095036995@qq.com>
2019-11-13 03:55:41 +0800

    fix

--- app/src/main/java/com/szwangel/habit/application/HabitApplication.java
+++ app/src/main/java/com/szwangel/habit/application/HabitApplication.java
@@ -3,9 +3,8 @@ package com.szwangel.habit.application;
 import android.app.Application;
 
 public class HabitApplication extends Application {
-    private String domain = "http://118.89.16.252/habit/"; // 正式服
 
     public String getDomain() {
-        return domain;
+        return "http://www.shunlushunlu.cn/habit/";
     }
 }
